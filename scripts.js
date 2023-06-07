1529fbe54bf7f7a8e888839169a8d34acb4eb384
Pranava Adiga <66902872+adigapranava@users.noreply.github.com>
2023-06-07 15:08:10 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,14 @@
 // Initialize Firebase
-var firebaseConfig = {
-    authDomain: "pythonimageupload-99434.firebaseapp.com",
-    apiKey: "AIzaSyBhGSwgsiF-6ayIUzRbd7y3rj9IS9WmC6c",
-    databaseURL: "https://pythonimageupload-99434-default-rtdb.firebaseio.com/",
-    projectId: "pythonimageupload-99434",
-    storageBucket: "pythonimageupload-99434.appspot.com",
-    serviceAccount: {
-        type: "service_account",
-        project_id: "pythonimageupload-99434",
-        private_key_id: "a206eed2141ec9062476ee1596553e121a1d013f",
-        private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCsK/Q6FTiBi7Z8\n8xxnIyGK+UrKB70aNLdUcihBOe0Xq7JZxrTvzSDgh8ArWW7OI5Bt3Jzr9ZDW5gFd\nimzZFHTYwBzB+DJR0BzwIvrdbzDPt588apO9rwbDGREdgCidxCaiIx0SMVWdlOez\nZAKiYNex1aXZkhOGQCxDl6DLa2+GFP3DNLWbpcGQQ1rYWL+U6BZqldNFW1fu8OZz\n2iDWlz2YnZolX/F38ekf37Sxj3teIM+Q2F37cWArWS3CYoOLKKW5AMunBfZ963s2\nxPEhMFJqYzDGLNapxllA+4Mi2jOZI9/amw4KWYRmS95vblyTfaka9g+qfgYKJt25\nDxwB9LUxAgMBAAECggEAPmcR9olheSaz5lbRORqVGkNf1Zj3c8uelQdpSrbdlcI/\n+uUYCIBvRaI+Rxf7ntBqFT+sSqOvMwybexVkfUu6TCNZaP518ow9KNZs2Zxm8yqJ\nFwND+DC7gTQRLYGZdxE0tXiHnWPvNZXWYwFBKSxzkTuwCIs282ZIBNmsyH3wpoZU\nZR4lt+4aYKK15oq3miV57f607nFxTZTWTaEWvhcQ5Z9q3Fj/4WUnSNS0Xr0RCbk7\nN4mlso7TUFVUNwCXFDDc2P6kBFieuHACxFCPTgISdBvBcR4SAG6RRSSGfrlfeejE\neBGoxHt3/LHIkHcVuiAs/EJLACfRvGBh0cxh5bM+TwKBgQDyy9BYrNkMQ9LQyXug\nC6jNvkAdwn9z9OAUqzTkrkR3BJmC498vI1mxCwfSzLkQiJz2/kYO01oLUPQ2IziA\noDrqJg0aHNdSemwg1lGmRhdkeXRDv5xJcubduID/nXyJJEX/zaDErDncLgMxzXAW\n7KRVjWGBYa+RS6bTAY27zHB52wKBgQC1iOmyZt3iTWG1kaVPow7hxtL226puOoCE\nazp8k8aSu06NQnw2x+w6BYHZly6W3KALq6WBrFsk3ByibxVA2iYf9LVFW1ZAjH3L\nIwKwdApLUdMsc6cn5XiI+GBvuyV1ssOKwqv65WzrmoF9YzapnAeN/nWYVFcWUPJx\nALa7f2544wKBgQDxuMaJsjQ5CME3CpEg6tq5U0MEHeLEMDRzf2wei/0vvQ2UiYdm\np7whDglQ7m8FLe2P3eEj23mXMJah23jiXiOpgA6lQ7d/rtA99QG/F6Msbdq5pGdV\nnKaKcalEU6dr5ggmv7Lie1Dzi3+0soJ44EpPvn0n0Xh4oCfLhY11Pflw5wKBgHO4\neEjaXB0zeNLWqkuuxQDVzovAkc88icpf1GOvjAjjT/qqZCZDoWmHMzQytGdE8t0d\nqR6VcXB4w+Qj7aduqJj8VEWHKN9y74l7ou46uoky+uu+OdMr639c3ZS79vAlaZNd\n72W7tIgn8N2uTh0msoYorGvTb8RxC2tPqiyx2glDAoGALbA2jZl+E4gO7Wk36HZH\nIbDwmBxvGaY2s+iDTvYajl09BBPWnJiZgxqCEKScWFjV456xtl4Xm/t5KA4koxYm\n1ryXSsOClT53Sdvf/hh/Z2uUqr0fmqlCWEV3OYoY+IBBUzEOLMxNy+ELwWe8zCbp\n3lhe9sx87sg9sZbGOF0aPO8=\n-----END PRIVATE KEY-----\n",
-        client_email: "firebase-adminsdk-wgrao@pythonimageupload-99434.iam.gserviceaccount.com",
-        client_id: "108008562746572805432",
-        auth_uri: "https://accounts.google.com/o/oauth2/auth",
-        token_uri: "https://oauth2.googleapis.com/token",
-        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-wgrao%40pythonimageupload-99434.iam.gserviceaccount.com"
-      }
-      
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBhGSwgsiF-6ayIUzRbd7y3rj9IS9WmC6c",
+  authDomain: "pythonimageupload-99434.firebaseapp.com",
+  databaseURL: "https://pythonimageupload-99434-default-rtdb.firebaseio.com",
+  projectId: "pythonimageupload-99434",
+  storageBucket: "pythonimageupload-99434.appspot.com",
+  messagingSenderId: "566073350313",
+  appId: "1:566073350313:web:e4c2edcc017ca22a6f71b8",
+  measurementId: "G-S9Q2TFVDYL"
+};
 
 firebase.initializeApp(firebaseConfig);
 
@@ -34,6 +24,7 @@ audio.muted = false;
 
 const visitorsContainer = document.querySelector('#visitors-container');
 const visitorsList = document.querySelector('#visitors-list');
+// console.log("hello");
 
 // accept visitor
 function acceptVisitor(visitorId){
@@ -78,12 +69,35 @@ function rejectVisitor(visitorId){
 
 
 var dbRef = DB.ref(USERNAME + "/visitorsList");
+// Retrieve the data from the database
+dbRef.once("value").then(function(snapshot) {
+  // Get the data snapshot
+  var data = snapshot.val();
+
+  // Check if there are any elements in the visitorsList
+  if (data) {
+    // Iterate over each element in the visitorsList
+    Object.keys(data).forEach(function(key) {
+      var element = data[key];
+      console.log(element);
+    });
+  } else {
+    console.log("No elements found in visitorsList");
+  }
+}).catch(function(error) {
+  console.log("Error retrieving data:", error);
+});
+
+
+
 dbRef.orderByKey().limitToLast(10).on('value', function(snapshot) {
     var listData = snapshot.val();
+    console.log(listData);
     visitorsList.innerHTML = ''; // Clear previous visitors list
     for (var key in listData) {
     if (listData.hasOwnProperty(key)) {
         var visitor = listData[key];
+        console.log(visitor);
         const card = document.createElement('div');
         card.classList.add('card', 'col-md-4');
 
@@ -106,7 +120,12 @@ dbRef.orderByKey().limitToLast(10).on('value', function(snapshot) {
         timeArrived.classList.add('card-text');
         timeArrived.textContent = getTimeElapsed(visitor.timeArrived);
         details.appendChild(timeArrived);
-        if (visitor.status && !visitor.acknowledged){
+        if (visitor.automated){
+          const viewButton = document.createElement('button');
+          viewButton.classList.add('btn', 'btn-primary', 'btn-block', 'disabled');
+          viewButton.textContent = 'Accepted | Auto';
+          details.appendChild(viewButton);
+        }else if (visitor.status && !visitor.acknowledged){
           const viewButton = document.createElement('button');
           viewButton.classList.add('btn', 'btn-success', 'btn-block');
           viewButton.textContent = 'Accept';
